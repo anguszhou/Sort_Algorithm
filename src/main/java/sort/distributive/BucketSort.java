685544aa1838eb139a465ab01315be57c3d934ec
zhou <zhoucong07@gmail.com>
2013-06-14 09:37:30 +0800

    Signed-off-by: zhou <zhoucong07@gmail.com>

--- src/main/java/sort/distributive/BucketSort.java
+++ src/main/java/sort/distributive/BucketSort.java
@@ -1,6 +1,7 @@
 package sort.distributive;
 
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 import sort.Sort;
 import sort.swap.QuickSort;
@@ -10,7 +11,21 @@ public class BucketSort extends Sort {
 	/**
 	 * @param args
 	 */
+	
+	private static  int i =0;
+	public int a(){
+		return  i++;
+	}
+	public int b(){
+		return ++i;
+	}
 	public static void main(String[] args) {
+		BucketSort a = new BucketSort();
+		System.out.println(a.a());
+		System.out.println(a.b());
+		BucketSort b = new BucketSort();
+		System.out.println(b.a());
+
 		// TODO Auto-generated method stub
 	/*	QuickSort qs = new QuickSort();
 		int[] data = qs.integerToInt(qs.getIntegerRandArray(10, 100));
@@ -23,7 +38,7 @@ public class BucketSort extends Sort {
 			System.out.print(data[i]+" , ");
 		}*/
 		
-		BucketSort bs = new BucketSort();
+	/*	BucketSort bs = new BucketSort();
 		int[] data = bs.integerToInt(bs.getIntegerRandArray(10, 100));
 		System.out.println("befort:");
 		for (int i = 0; i < data.length; i++) {
@@ -34,7 +49,7 @@ public class BucketSort extends Sort {
 		bs.bucketSort(data);
 		for (int i = 0; i < data.length; i++) {
 			System.out.print(data[i]+" , ");
-		}
+		}*/
 	}
 	
 	class Node{
